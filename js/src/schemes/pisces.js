e8643129a7c1356d412d3e8d4da906ca535868d8
xwdj <2374781080@qq.com>
2018-08-08 16:51:19 +0800

    Site updated: 2018-08-08 16:51:17

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -82,5 +82,32 @@ jQuery(document).ready(function($) {
         });
     });
 });
+
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
+
+<!-- 提示开启flash -->
+var myFlash = (function(){
+  if(typeof window.ActiveXObject != "undefined"){
+    return new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
+  }else{
+    return navigator.plugins['Shockwave Flash'];
+  }
+})();
   
 });
